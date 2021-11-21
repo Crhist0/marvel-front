feed0f6f4c0d7c2902255e99bf2c2592d1716cdf
Crhistian <crhistiandeoliveira@gmail.com>
2021-11-20 23:48:10 -0300

    adjusts on alignment

--- public/assets/js/apiSearch.js
+++ public/assets/js/apiSearch.js
@@ -1,5 +1,5 @@
 const api = axios.create({
-  baseURL: "https://api-backend-marvel-crhist0.herokuapp.com",
+    baseURL: "https://api-backend-marvel-crhist0.herokuapp.com",
 });
 
 const params = new URLSearchParams(window.location.search);
@@ -86,28 +86,28 @@ const params = new URLSearchParams(window.location.search);
 // );
 
 function listaPersonagens() {
-  let page = params.has("page") ? params.get("page") : "1";
-  let name = document.getElementById("searchByName").value;
-  api
-    .get("/", {
-      params: { page, name },
+    let page = params.has("page") ? params.get("page") : "1";
+    let name = document.getElementById("searchByName").value;
+    let limit = 25;
+    api.get("/", {
+        params: { page, limit, name },
     })
-    .then((result) => {
-      console.log(result.data);
+        .then((result) => {
+            console.log(result.data);
 
-      const lista = result.data.data; // test page
-      const searchResults = result.data.searchResults;
-      const detailsPageResults = result.data.detailsPageResults;
-      const copy = result.data.copy; // mensagem do final da pagina
+            const lista = result.data.data; // test page
+            const searchResults = result.data.searchResults;
+            const detailsPageResults = result.data.detailsPageResults;
+            const copy = result.data.copy; // mensagem do final da pagina
 
-      atualizaTabela("#searchCaracter", lista, detailsPageResults);
-    })
-    .catch((err) => {
-      console.log("erro?");
-      console.log(err);
-      console.log(err.request);
-      console.log(err.result);
-    });
+            atualizaTabela("#searchCaracter", lista, detailsPageResults);
+        })
+        .catch((err) => {
+            console.log("erro?");
+            console.log(err);
+            console.log(err.request);
+            console.log(err.result);
+        });
 }
 // function AllListCharacter(nameH, numero, idHtmlTag) {
 //   let page = params.has("page") ? params.get("page") : numero;
@@ -138,44 +138,34 @@ function listaPersonagens() {
 //   window.location.href = `http://127.0.0.1:5501/public/searchResults.html?page=${num}`; // trocar para o link do heroku
 // }
 function atualizaTabela(id, lista, detailsPageResults) {
-  let list = document.querySelector(id);
-  list.style.display = "block";
-  //   const tbodyLista = document.querySelector("#searchCaracter > div");
-  console.log(detailsPageResults);
-  list.innerHTML = "";
+    let list = document.querySelector(id);
+    list.style.display = "block";
+    //   const tbodyLista = document.querySelector("#searchCaracter > div");
+    console.log(detailsPageResults);
+    list.innerHTML = "";
 
-  for (const personagem of lista) {
-    let thumb = personagem.thumbnail;
-    // link para página de detalhes, em geral é a mesma que a comiclink page
-    let details =
-      personagem.details == -1 || personagem.details == ""
-        ? ""
-        : `<p><a href="${personagem.details}">details</a></p>`;
-    // link para página wiki quando possui
-    let wiki =
-      personagem.wiki == -1 || personagem.wiki == ""
-        ? ""
-        : `<p><a href="${personagem.wiki}">wiki</a></p>`;
-    // link para página de "comiclink", em geral é a mesma da de details
-    let comiclink =
-      personagem.comiclink == -1 || personagem.comiclink == ""
-        ? ""
-        : `<p><a href="${personagem.comiclink}">comiclink</a></p>`;
+    for (const personagem of lista) {
+        let thumb = personagem.thumbnail;
+        // link para página de detalhes, em geral é a mesma que a comiclink page
+        let details = personagem.details == -1 || personagem.details == "" ? "" : `<p><a href="${personagem.details}">details</a></p>`;
+        // link para página wiki quando possui
+        let wiki = personagem.wiki == -1 || personagem.wiki == "" ? "" : `<p><a href="${personagem.wiki}">wiki</a></p>`;
+        // link para página de "comiclink", em geral é a mesma da de details
+        let comiclink = personagem.comiclink == -1 || personagem.comiclink == "" ? "" : `<p><a href="${personagem.comiclink}">comiclink</a></p>`;
 
-    if (
-      personagem.thumbnail ==
-        "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg" ||
-      personagem.thumbnail == `http://i.annihil.us/u/prod/marvel/i/mg/f/60/4c002e0305708.gif` ||
-      personagem.thumbnail == "insira aqui outra imagem para exceção"
-    ) {
-      // se for as imagens de "not found", mostra a logo marvel abaixo
-      thumb = `https://i.ibb.co/vLhYShQ/Screenshot-1.png`;
-    }
-    list.innerHTML += `
-    <div class="item mt-2">
-      <div class="card clickHere" style="width: 10rem; height: 10.2rem">
+        if (
+            personagem.thumbnail == "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg" ||
+            personagem.thumbnail == `http://i.annihil.us/u/prod/marvel/i/mg/f/60/4c002e0305708.gif` ||
+            personagem.thumbnail == "insira aqui outra imagem para exceção"
+        ) {
+            // se for as imagens de "not found", mostra a logo marvel abaixo
+            thumb = `https://i.ibb.co/vLhYShQ/Screenshot-1.png`;
+        }
+        list.innerHTML += `
+    <div class="item m-4">
+      <div class="card clickHere d-flex justify-content-center" style="width: 15rem; height: 15.2rem">
          
-            <img   src="${thumb}" class="card-img-top" alt="${personagem.name}" style="height: 10rem">
+            <img   src="${thumb}" class="card-img-top" alt="${personagem.name}" style="height: 15rem">
           
             <div class="card-body scrollable-element opacity_0_1" style="overflow: auto;" >
               <p class="card-text" >
@@ -188,9 +178,9 @@ function atualizaTabela(id, lista, detailsPageResults) {
             </div>
       </div>
     </div>`;
-  }
+    }
 
-  // console.log(searchResults);
+    // console.log(searchResults);
 
-  //   document.getElementById("footer").innerHTML = copy;
+    //   document.getElementById("footer").innerHTML = copy;
 }
